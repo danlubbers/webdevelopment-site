f5bc4491b59cdb334df0ab16218780c8b6364e3c
Dan Lubbers <lubbers.dan@gmail.com>
2018-05-26 17:14:08 -0600

    added react mailto for hyperlink contact

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Logo from '../../images/danlubbers-logo.png'
 import Hamburger from 'react-icons/lib/fa/bars'
+import Mailto from 'react-mailto'
 
 const Header = () => (
   <div className='header'>
@@ -9,7 +10,7 @@ const Header = () => (
       <ul className='desktop-nav'>
         {/* <li>ABOUT</li>
         <li>WEBDEV</li> */}
-        <li>CONTACT</li>
+        <Mailto  className='contact' email="lubbers.dan@gmail.com" obfuscate={true}><li>CONTACT</li></Mailto>
       </ul>
     </nav> 
     <div className='mobile-nav'>
