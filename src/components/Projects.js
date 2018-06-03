8fcf76d6d5aeef6eb6087ea4f5fb37537a278724
Dan Lubbers <lubbers.dan@gmail.com>
2018-06-03 15:31:16 -0600

    added AOS animation to logos and images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,20 @@
 import React, {Component} from 'react'
+// AOS
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+// Screenshots
 import ArcDesk from '../../images/arc-clone-desktop.gif'
 import ArcMobile from '../../images/arc-clone-mobile.png'
 
 export default class Projects extends Component {
+    constructor(props) {
+        super(props)
+    }
+  
+      componentDidMount() {
+        this.aos = AOS;
+        this.aos.init({duration: 1000});
+    }
     render() {
         return(
             <div className='projects-container'>
@@ -17,8 +29,8 @@ export default class Projects extends Component {
                         I chose to clone an e-commerce site. Cloning this site allowed me to showcase that I can take a design and recreate the layout into a full functioning site. I used CSS3/SASS for all of the styling and to make the design mobile responsive for iphone 6, 7, 8 and desktop. I used React.js to build the site with the use of Javascript along with Redux to manage the "state." I used PostgreSQL and Heroku for database management of the products. The Domain is registered and maintained through Hover and the site is hosted on Digital Ocean.
                     </p>
                     <div className='screenshots'>
-                   <a href='http://arcteryxclone.danlubbers.com' target='_blank'><img className='arc-desk' src={ArcDesk} alt='arcteryx clone desktop screenshot'/></a>
-                   <a href='http://arcteryxclone.danlubbers.com' target='_blank'><img className='arc-mobile' src={ArcMobile} alt='arcteryx clone mobile screenshot'/></a>
+                   <a href='http://arcteryxclone.danlubbers.com' target='_blank'><img data-aos='zoom-in' data-aos-anchor-placement="top-center" className='arc-desk' src={ArcDesk} alt='arcteryx clone desktop screenshot'/></a>
+                   <a href='http://arcteryxclone.danlubbers.com' target='_blank'><img data-aos='zoom-in' data-aos-anchor-placement="top-center" className='arc-mobile' src={ArcMobile} alt='arcteryx clone mobile screenshot'/></a>
                     </div>
                 </section>
             </div>
--- src/components/Skillset.js
+++ src/components/Skillset.js
@@ -1,4 +1,8 @@
 import React, {Component} from 'react'
+// AOS
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+// Logos
 import gitLogo from '../../images/git-logo.png'
 import githubLogo from '../../images/github-logo.svg'
 import reactLogo from '../../images/react-icon.svg.png'
@@ -15,6 +19,19 @@ import PSLogo from '../../images/ps-logo.png'
 import heliconLogo from '../../images/helicon-logo.png'
 
 export default class Skillset extends Component {
+    constructor(props) {
+      super(props)
+    }
+
+    componentDidMount() {
+      this.aos = AOS;
+      this.aos.init({duration: 1000});
+  }
+
+  // componentDidUpdate() {
+  //     this.aos.refresh();
+  // }
+
     render() {
         return(
             <div className='skillset-content'>
@@ -23,59 +40,60 @@ export default class Skillset extends Component {
               {/* <h2 className='skillset'>SKILL SET</h2> */}
             </div>
             <div className="logo-container">
-              <div>
+              <div data-aos='fade-up' data-aos-anchor-placement="center-center">
                 <img className='logos' src={gitLogo} alt='git logo'/>
                 <h2 className='logo-text'>GIT</h2>
               </div>
-              <div>
+              <div data-aos='fade-up' data-aos-anchor-placement="center-center" data-aos-delay='100'>
                 <img className='logos' src={githubLogo} alt='github logo'/>
                 <h2 className='logo-text'>GITHUB</h2>
               </div>
-              <div>
+              <div data-aos='fade-up' data-aos-anchor-placement="center-center" data-aos-delay='200'>
                 <img className='logos' src={reactLogo} alt='react logo'/>
                 <h2 className='logo-text'>REACT</h2>
               </div>
-              <div>
+              <div data-aos='fade-up' data-aos-anchor-placement="center-center" data-aos-delay='300'>
                 <img className='logos' src={html5Logo} alt='html5 logo'/>
                 <h2 className='logo-text'>HTML5</h2>
               </div>
-              <div>
+              <div data-aos='fade-up' data-aos-anchor-placement="center-center" 
+              data-aos-delay='400'>
                 <img className='logos' src={css3Logo} alt='css3 logo'/>
                 <h2 className='logo-text'>CSS3</h2>
               </div>
-              <div>
+              <div data-aos='fade-up' data-aos-anchor-placement="center-center">
                 <img className='logos' src={sassLogo} alt='sass logo'/>
                 <h2 className='logo-text'>SASS</h2>
               </div>
-              <div>
+              <div data-aos='fade-up' data-aos-anchor-placement="center-center" data-aos-delay='100'>
                 <img className='logos' src={JSLogo} alt='JS logo'/>
                 <h2 className='logo-text'>JAVASCRIPT</h2>
               </div>
-              <div>
+              <div data-aos='fade-up' data-aos-anchor-placement="center-center" data-aos-delay='200'>
                 <img className='logos' src={reduxLogo} alt='redux logo'/>
                 <h2 className='logo-text'>REDUX</h2>
               </div>
-              <div>
+              <div data-aos='fade-up' data-aos-anchor-placement="center-center" data-aos-delay='300'>
                 <img className='logos' src={nodeLogo} alt='node logo'/>
                 <h2 className='logo-text'>NODE</h2>
               </div>
-              <div>
+              <div data-aos='fade-up' data-aos-anchor-placement="center-center" data-aos-delay='400'>
                 <img className='logos' src={postgresLogo} alt='postgres logo'/>
                 <h2 className='logo-text'>POSTGRESQL</h2>
               </div>
-              <div>
+              <div data-aos='fade-up' data-aos-anchor-placement="center-center">
                 <img className='logos' src={captureOneLogo} alt='captureOne logo'/>
                 <h2 className='logo-text'>CAPTUREONE</h2>
               </div>
-              <div>
+              <div data-aos='fade-up' data-aos-anchor-placement="center-center" data-aos-delay='100'>
                 <img className='logos' src={heliconLogo} alt='Helicon Focus logo'/>
                 <h2 className='logo-text'>HELICON FOCUS</h2>
               </div>
-              <div>
+              <div data-aos='fade-up' data-aos-anchor-placement="center-center" data-aos-delay='200'>
                 <img className='logos' src={LRLogo} alt='Adobe Lightroom logo'/>
                 <h2 className='logo-text'>ADOBE LIGHTROOM</h2>
               </div>
-              <div>
+              <div data-aos='fade-up' data-aos-anchor-placement="center-center" data-aos-delay='300'>
                 <img className='logos' src={PSLogo} alt='Adobe Photoshop logo'/>
                 <h2 className='logo-text'>ADOBE PHOTOSHOP</h2>
               </div>
