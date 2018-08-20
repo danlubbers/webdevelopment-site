429ca3ad4da2c908ef204a05ff82b33b23c4e635
Dan Lubbers <lubbers.dan@gmail.com>
2018-08-20 13:58:36 -0400

    Adding metadata for SEO

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -9,9 +9,11 @@ const Layout = ({ children, data }) => (
   <div>
     <Helmet
       title='Dan Lubbers | Full Stack Web Developer'
+      shortName='Dan Lubbers'
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        {author: 'Dan Lubbers'},
+        { name: 'description', content: 'Dan Lubbers is a Photographer / Retoucher / Web Developer specializing in Studio and Environmental Portraits in Saratoga Springs, NY.' },
+        { name: 'keywords', content: 'dan lubbers, web, web developer, developer, development, full stack, coder, javascript, react, reactjs, programmer, photographer, photography, photographs, saratoga, springs, ny, new york, louisville, kentucky, ky, fine art, fine art portraiture, editorial, portraits, portraiture, environmental portraits, studio, product, people, emotion, passion, adventure, action, lifestyle, scenic, landscape, iphoneography, music, musicians, will, oldham, jim, james, my morning jacket, mmj, bonnie, prince, billy, sport, sports, red river gorge, rock climbing, climbing, climber, rockclimbing, headshots, commercial, cycling, trailrunning, running, athletes, professional, athletic, local' },
       ]}
     />
     <Header />
