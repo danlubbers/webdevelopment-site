cee0f780996ffc775d8a5a07851113501a60798f
Dan Lubbers <lubbers.dan@gmail.com>
2018-08-20 13:48:50 -0400

    added rel noopener to target blank links so cross origin desintaions are safe

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -33,10 +33,10 @@ export default class Projects extends Component {
                     <p>
                         I chose to clone an e-commerce site. Cloning this site allowed me to showcase that I can take a design and recreate the layout into a full functioning site. I used CSS3/SASS for all of the styling and to make the design mobile responsive for iphone 6, 7, 8 and desktop. I used React.js to build the site with the use of Javascript along with Redux to manage the "state." I used PostgreSQL and Heroku for database management of the products. The Domain is registered and maintained through Hover and the site is hosted on Digital Ocean.
                     </p>
-                    <a href='https://github.com/danlubbers/arcteryx-clone' target='_blank'>GITHUB REPO</a>
+                    <a href='https://github.com/danlubbers/arcteryx-clone' target='_blank' rel="noopener">GITHUB REPO</a>
                     <div className='screenshots'>
-                   <a href='http://arcteryxclone.danlubbers.com' target='_blank'><img data-aos='zoom-in' data-aos-anchor-placement="top-center" className='arc-desk' src={ArcDesk} alt='arcteryx clone desktop screenshot'/></a>
-                   <a href='http://arcteryxclone.danlubbers.com' target='_blank'><img data-aos='zoom-in' data-aos-anchor-placement="top-center" className='arc-mobile' src={ArcMobile} alt='arcteryx clone mobile screenshot'/></a>
+                   <a href='http://arcteryxclone.danlubbers.com' target='_blank' rel="noopener"><img data-aos='zoom-in' data-aos-anchor-placement="top-center" className='arc-desk' src={ArcDesk} alt='arcteryx clone desktop screenshot'/></a>
+                   <a href='http://arcteryxclone.danlubbers.com' target='_blank' rel="noopener"><img data-aos='zoom-in' data-aos-anchor-placement="top-center" className='arc-mobile' src={ArcMobile} alt='arcteryx clone mobile screenshot'/></a>
                     </div>
                 </section>
 
@@ -50,9 +50,9 @@ export default class Projects extends Component {
                     <p>
                     Our group wanted to clone an e-commerce site. Cloning this site allowed us to showcase that we can take a design and recreate the layout into a full functioning site. We used CSS3/SASS for all of the styling. We used React.js to build the site with the use of Javascript along with Redux to manage the "state." We used PostgresSQL and Heroku for database management of the products. The Domain is registered and maintained through Hover and the site is hosted on Digital Ocean.
                     </p>
-                    <a href='https://github.com/danlubbers/ruffwear-clone' target='_blank'>GITHUB REPO</a>
+                    <a href='https://github.com/danlubbers/ruffwear-clone' target='_blank' rel="noopener">GITHUB REPO</a>
                     <div className='screenshots'>
-                   <a href='http://ruffdoggies.danlubbers.com' target='_blank'><img data-aos='zoom-in' data-aos-anchor-placement="bottom" className='ruff-desk' src={RuffDesk} alt='ruffwear clone desktop screenshot'/></a>
+                   <a href='http://ruffdoggies.danlubbers.com' target='_blank' rel="noopener"><img data-aos='zoom-in' data-aos-anchor-placement="bottom" className='ruff-desk' src={RuffDesk} alt='ruffwear clone desktop screenshot'/></a>
                     </div>
                 </section>
 
@@ -66,9 +66,9 @@ export default class Projects extends Component {
                     <p>
                     Client/Artist, Sunny Ra wanted a complete redesign of her portfolio site. I created the wireframe and designed the layout and color scheme. I photographed her portrait for the 'About' section. I then built her site using Reactjs. I made the site fully responsive using Media Queries in SASS. I used React Helmet so google would be able to crawl her site searching for keywords helping SEO (Seach Engine Optimization). I added Google Analytics so Sunny can track her traffic going to her new site.  
                     </p>
-                    <a href='https://github.com/danlubbers/sunny-ra' target='_blank'>GITHUB REPO</a>
+                    <a href='https://github.com/danlubbers/sunny-ra' target='_blank' rel="noopener">GITHUB REPO</a>
                     <div className='screenshots'>
-                   <a href='http://sunny-ra.com' target='_blank'><img data-aos='zoom-in' data-aos-anchor-placement="bottom" className='sunny-desk' src={SunnyRa} alt='sunny ra portfolio site shown on different devices'/></a>
+                   <a href='http://sunny-ra.com' target='_blank' rel="noopener"><img data-aos='zoom-in' data-aos-anchor-placement="bottom" className='sunny-desk' src={SunnyRa} alt='sunny ra portfolio site shown on different devices'/></a>
                     </div>
                 </section>
             </div>
