89beb83ba1cde88ff13df651f91e478e49f2ab76
Dan Lubbers <lubbers.dan@gmail.com>
2019-09-09 17:20:38 -0400

    Updated refactored Sunny Ra Website image and description

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -58,11 +58,12 @@ export default class Projects extends Component {
 
                     <h1 className='sunny-project-text'>CLIENT PORTFOLIO SITE</h1>
                     <h2 className='sunny-text'>ARTIST SUNNY RA</h2>
-                    <p className='sunny-tech'>TECH USED: REACT | SASS | JAVASCRIPT 
-                    | NETLIFY | REACT HELMET | GOOGLE ANALYTICS
+                    <p className='sunny-tech'>Tech used: 
+  ReactJS | HTML | Sass | Javascript | Redux | React Helmet | PostgresSQL | Digital Ocean | Google Analytics
                 </p>
                 <section>
                 <h3>DETAILS OF PROJECT :</h3>
+                    <p>*UPDATE* Refactored code Aug. 2019 to cut out 75% of static code and converted it over to a SQL database and hosting on Digital Ocean. </p>
                     <p>
                     Client/Artist, Sunny Ra wanted a complete redesign of her portfolio site. I created the wireframe and designed the layout and color scheme. I photographed her portrait for the 'About' section. I then built her site using Reactjs. I made the site fully responsive using Media Queries in SASS. I used React Helmet so google would be able to crawl her site searching for keywords helping SEO (Seach Engine Optimization). I added Google Analytics so Sunny can track her traffic going to her new site.  
                     </p>
